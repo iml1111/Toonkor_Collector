7011c3b4d407f28c074ec60ed3a2cd73235b08e2
iml1111 <shin10256@gmail.com>
2019-05-09 21:26:51 +0900

    v.2 add

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from cx_Freeze import setup, Executable
 import sys, os
 import requests.certs
-
-includeFile = [(requests.certs.where(), "cacert.pem"),
-"C:\\Users\\IML\\AppData\\Local\\Programs\\Python\\Python37-32\\DLLs\\tcl86t.dll", 
-"C:\\Users\\IML\\AppData\\Local\\Programs\\Python\\Python37-32\\DLLs\\tk86t.dll"]
-
 os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(os.getcwd(), "cacert.pem")
 
 buildOptions = dict(
 	packages = ["PyQt5","sys","bs4","requests","selenium","os","queue",\
     "PIL","idna.idnadata","shutil","threading","xml","ssl"], 
-	excludes = ["tkinter", "sqlite3"],
-    include_files = includeFile)
+	excludes = ["tkinter", "sqlite3"])
 
 base = None
 if sys.platform == "win32": #windows GUI 일경우 
